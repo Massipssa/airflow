c45578381500332e0cb80aba147bad041dcf8e76
Massipssa Kerrache <kerrache.massipssa@gmail.com>
2020-07-10 16:25:23 +0200

    update travis.yml

--- scripts/docker-compose/docker-image.sh
+++
@@ -1,7 +0,0 @@
-function push_image () {
-    docker push 
-}
-
-function build_image() {
-    docker build -f /path/to/a/Dockerfile -t image_repo/image_tag
-}
---
+++ scripts/docker-image.sh
@@ -0,0 +1,23 @@
+
+IMAGE=$USERNAME/$IMAGE_NAME:$VERSION
+
+case $1 in
+  push)
+    push_image
+  ;;
+  build)
+    build_image
+esac
+
+function push_image {
+  echo "Start building image"
+  echo
+  docker login -u "$USERNAME" -p "$PASSWORD"
+  docker build -t "$IMAGE" .
+  docker push "$IMAGE"
+  echo "Image $IMAGE push to Docker Hub"
+}
+
+function build_image() {
+    docker build -t IMAGE
+}
